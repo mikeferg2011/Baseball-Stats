8df3fc8da473258ba41965614dfee8b01d91674a
Michael Ferguson <mikeferg2011@gmail.com>
2024-09-01 21:49:56 -0400

    update local gcp project start script

--- gcloud_api_commands.sh
+++ gcloud_api_commands.sh
@@ -23,18 +23,28 @@ declare -a arr=("roles/workflows.editor"
                 "roles/bigquery.dataOwner"
                 "roles/bigquery.user"
                 "roles/bigquery.admin"
+                "roles/cloudfunctions.developer"
+                "roles/cloudbuild.builds.editor"
                 )
 
 for i in "${arr[@]}"
 do
    echo "$i"
    gcloud projects add-iam-policy-binding baseball-434300 \
-        --member "serviceAccount:baseball-434300@appspot.gserviceaccount.com" \
-        --role "$i"
+      --member "serviceAccount:baseball-434300@appspot.gserviceaccount.com" \
+      --role "$i"
 #    gcloud services enable "$i"
 done
 
 gcloud iam service-accounts keys create sa_key.json \
-    --iam-account=baseball-434300@appspot.gserviceaccount.com
-# gcloud workflows deploy myFirstWorkflow --source=myFirstWorkflow.yaml \
-#     --service-account=baseball-434300@appspot.gserviceaccount.com
+   --iam-account=baseball-434300@appspot.gserviceaccount.com
+
+gcloud builds triggers create github \
+   --name="master-trigger" \
+   --repo-name="Baseball-Stats" \
+   --repo-owner="mikeferg2011" \
+   --branch-pattern="^master$" \
+   --build-config="cloudbuild.yaml" \
+   --service-account="projects/baseball-434300/serviceAccounts/baseball-434300@appspot.gserviceaccount.com" \
+   --require-approval \
+   --include-logs-with-status
