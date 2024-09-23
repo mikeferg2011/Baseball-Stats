0a4f4be4e0c89c5a81f7341ae11b544aa072e6a5
Michael Ferguson <mikeferg2011@gmail.com>
2024-09-22 21:29:14 -0400

    rewrite bq writer

--- bigquery_loader/main.py
+++ bigquery_loader/main.py
@@ -1,31 +1,5 @@
 import functions_framework
-# from urllib.request import urlopen
-import zipfile
-from io import BytesIO
-# import pandas as pd
-import pandas_gbq
-# from markupsafe import escape
-from google.cloud import storage
-from parse_helper import RetroParser
-
-BASE_URL = 'https://www.retrosheet.org'
-PROJECT_ID = 'baseball-434918'
-BUCKET_NAME = 'retrosheets'
-
-
-# def convert_num_to_date(df_series):
-#     temp_series = df_series.astype('Int64').astype('str')
-#     temp_series = pd.to_datetime(temp_series, format="%Y%m%d", errors='coerce')
-#     temp_series = temp_series.dt.date.astype('str').replace('NaT', None)
-#     return (temp_series)
-
-
-def read_zip_gcs(blob_name):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(BUCKET_NAME)
-    blob = bucket.blob(blob_name)
-    myzip = zipfile.ZipFile(BytesIO(blob.download_as_string()))
-    return(myzip)
+import parse_helper as ph
 
 
 # @functions_framework.http
@@ -57,108 +31,24 @@ def read_zip_gcs(blob_name):
 #         raise ValueError(f"Unknown content type: {content_type}")
 #     return f"Hello {escape(name)}!"
 
+FILE_DICT = {
+    'ballparks': ph.BallparkParser,
+    'teams': ph.TeamsParser,
+    'bios': ph.PeopleParser,
+}
+
 
 @functions_framework.http
 def process_zip(request):
     content_type = request.headers['content-type']
-    if content_type == 'application/json':
-        request_json = request.get_json(silent=True)
-        if request_json and 'file_path' in request_json and 'file_type' in request_json:
-            file_path = request_json['file_path']
-            file_type = request_json['file_type']
-        else:
-            raise ValueError("JSON is invalid, missing a 'file_path' property, or , missing a 'file_type' property")
-    else:
-        raise ValueError(f"Content type requires 'application/json' but given '{content_type}'")
-    myzip = read_zip_gcs(file_path)
-    df, mode = RetroParser(zip_bytes=myzip, zip_type=file_type).process()
-    pandas_gbq.to_gbq(df, f'retrosheets.{file_type}', project_id=PROJECT_ID, if_exists=mode)
+    assert content_type == 'application/json', f'Content type requires "application/json" but given "{content_type}"'
+
+    request_json = request.get_json()
+    file_path = request_json['file_path']
+    data_type = request_json['data_type']
+    # TODO make write mode optional in pipeline call
+    write_mode = request_json.get('write_mode')
+    
+    # add if on data_type 
+    FILE_DICT[data_type](file_path).pipeline()
     return f'{file_path} loaded successfully'
-
-
-# @functions_framework.http
-# def load_people(request):
-#     myzip = read_zip_gcs('biofile.zip')
-#     print(myzip.namelist())
-#     with myzip as z:
-#         # open the csv file in the dataset
-#         with z.open("biofile0.csv") as f:
-#             df = pd.read_csv(f)
-#             col_list = [
-#                 'id', 'lastname', 'usename', 'fullname',
-#                 'birthdate', 'birthcity', 'birthstate', 'birthcountry',
-#                 'deathdate', 'deathcity', 'deathstate', 'deathcountry',
-#                 # 'cemetery', 'cem_city', 'cem_state', 'cem_ctry', 'cem_note',
-#                 'birthname', 'altname',
-#                 'debut_p', 'last_p',
-#                 # 'debut_c', 'last_c',
-#                 # 'debut_m', 'last_m',
-#                 # 'debut_u', 'last_u',
-#                 'bats', 'throws',
-#                 'height', 'weight',
-#                 'HOF'
-#             ]
-
-#             df = df[col_list]
-
-#             df = df.rename(columns={
-#                 'id': 'id', 'lastname': 'last_name', 'usename': 'first_name', 'fullname': 'full_name',
-#                 'birthdate': 'birth_date', 'birthcity': 'birth_city', 'birthstate': 'birth_state', 'birthcountry': 'birth_country',
-#                 'deathdate': 'death_date', 'deathcity': 'death_city', 'deathstate': 'death_state', 'deathcountry': 'death_country',
-#                 'birthname': 'birth_name', 'altname': 'alt_name',
-#                 'debut_p': 'debut_player', 'last_p': 'last_player',
-#                 'bats': 'bats', 'throws': 'throws',
-#                 'height': 'height', 'weight': 'weight',
-#                 'HOF': 'hof',
-#             })
-
-#             df.birth_date = convert_num_to_date(df.birth_date)
-#             df.death_date = convert_num_to_date(df.death_date)
-
-#             df.debut_player = convert_num_to_date(df.debut_player)
-#             df.last_player = convert_num_to_date(df.last_player)
-#             pandas_gbq.to_gbq(df, 'retrosheets.persons', project_id=PROJECT_ID, if_exists='replace')
-#             return ("People loaded successfully")
-
-# @functions_framework.http
-# def load_ballparks(request):
-#     myzip = read_zip_gcs('ballparks.zip')
-#     print(myzip.namelist())
-#     with myzip as z:
-#         # open the csv file in the dataset
-#         with z.open("ballparks.csv") as f:
-#             df = pd.read_csv(f)
-#             df = df.rename(columns={
-#                 'PARKID': 'park_id',
-#                 'NAME': 'name',
-#                 'AKA': 'alt_names',
-#                 'CITY': 'city',
-#                 'STATE': 'state',
-#                 'START': 'start_dt',
-#                 'END': 'end_dt',
-#                 'LEAGUE': 'league',
-#                 'NOTES': 'notes',
-#             })
-#             df.start_dt = pd.to_datetime(df.start_dt, format='%m/%d/%Y').astype('str').replace('NaT', None)
-#             df.end_dt = pd.to_datetime(df.end_dt, format='%m/%d/%Y').astype('str').replace('NaT', None)
-#             pandas_gbq.to_gbq(df, 'retrosheets.ballparks', project_id=PROJECT_ID, if_exists='replace')
-#             return ("Stadiums loaded successfully")
-
-# @functions_framework.http
-# def load_teams(request):
-#     myzip = read_zip_gcs('teams.zip')
-#     print(myzip.namelist())
-#     with myzip as z:
-#         # open the csv file in the dataset
-#         with z.open("teams.csv") as f:
-#             df = pd.read_csv(f)
-#             df = df.rename(columns={
-#                 'TEAM': 'team_id',
-#                 'LEAGUE': 'league',
-#                 'CITY': 'city',
-#                 'NICKNAME': 'nickname',
-#                 'FIRST': 'first_year',
-#                 'LAST': 'last_year',
-#             })
-#             pandas_gbq.to_gbq(df, 'retrosheets.teams', project_id=PROJECT_ID, if_exists='replace')
-#             return ("Teams loaded successfully")
--- bigquery_loader/parse_helper.py
+++ bigquery_loader/parse_helper.py
@@ -1,4 +1,13 @@
 import pandas as pd
+import pandas_gbq
+import zipfile
+from io import BytesIO
+from google.cloud import storage
+from abc import ABC, abstractmethod
+
+
+PROJECT_ID = 'baseball-434918'
+BUCKET_NAME = 'retrosheets'
 
 
 def convert_num_to_date(df_series):
@@ -8,94 +17,126 @@ def convert_num_to_date(df_series):
     return (temp_series)
 
 
-class RetroParser:
-    def __init__(self, zip_bytes, zip_type):
-        self.zip_bytes = zip_bytes
-        self.zip_type = zip_type
+class BaseParser(ABC):
+    def __init__(self, blob_name, tbl_dest, write_mode):
+        self.blob_name = blob_name
+        self.tbl_dest = tbl_dest
+        self.write_mode = write_mode
 
-    def process_ballparks(self):
-        with self.zip_bytes as z:
+    def read_zip_gcs(self):
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(BUCKET_NAME)
+        blob = bucket.blob(self.blob_name)
+        self.zip = zipfile.ZipFile(BytesIO(blob.download_as_string()))
+
+    @abstractmethod
+    def process(self):
+        pass
+
+    def bq_write(self):
+        pandas_gbq.to_gbq(self.df,
+                          f'retrosheets.{self.tbl_dest}',
+                          project_id=PROJECT_ID,
+                          if_exists=self.write_mode
+                          )
+
+    def pipeline(self):
+        self.read_zip_gcs()
+        self.process()
+        self.bq_write()
+
+
+class BallparkParser(BaseParser):
+
+    def __init__(self, blob_name, tbl_dest='ballparks', write_mode='replace'):
+        super().__init__(self, blob_name, tbl_dest, write_mode)
+
+    def process(self):
+        with self.zip as z:
             # open the csv file in the dataset
-            with z.open("ballparks.csv") as f:
+            with z.open('ballparks.csv') as f:
                 df = pd.read_csv(f)
-                df = df.rename(columns={
-                    'PARKID': 'park_id',
-                    'NAME': 'name',
-                    'AKA': 'alt_names',
-                    'CITY': 'city',
-                    'STATE': 'state',
-                    'START': 'start_dt',
-                    'END': 'end_dt',
-                    'LEAGUE': 'league',
-                    'NOTES': 'notes',
-                })
-                df.start_dt = pd.to_datetime(df.start_dt, format='%m/%d/%Y').astype('str').replace('NaT', None)
-                df.end_dt = pd.to_datetime(df.end_dt, format='%m/%d/%Y').astype('str').replace('NaT', None)
-        return df
 
-    def process_bios(self):
-        with self.zip_bytes as z:
+        df = df.rename(columns={
+            'PARKID': 'park_id',
+            'NAME': 'name',
+            'AKA': 'alt_names',
+            'CITY': 'city',
+            'STATE': 'state',
+            'START': 'start_dt',
+            'END': 'end_dt',
+            'LEAGUE': 'league',
+            'NOTES': 'notes',
+        })
+        df.start_dt = pd.to_datetime(df.start_dt, format='%m/%d/%Y').astype('str').replace('NaT', None)
+        df.end_dt = pd.to_datetime(df.end_dt, format='%m/%d/%Y').astype('str').replace('NaT', None)
+        self.df = df
+
+
+class PeopleParser(BaseParser):
+    def __init__(self, blob_name, tbl_dest='bios', write_mode='replace'):
+        super().__init__(self, blob_name, tbl_dest, write_mode)
+
+    def process(self):
+        with self.zip as z:
             # open the csv file in the dataset
-            with z.open("biofile0.csv") as f:
+            with z.open('biofile0.csv') as f:
                 df = pd.read_csv(f)
-                col_list = [
-                    'id', 'lastname', 'usename', 'fullname',
-                    'birthdate', 'birthcity', 'birthstate', 'birthcountry',
-                    'deathdate', 'deathcity', 'deathstate', 'deathcountry',
-                    # 'cemetery', 'cem_city', 'cem_state', 'cem_ctry', 'cem_note',
-                    'birthname', 'altname',
-                    'debut_p', 'last_p',
-                    # 'debut_c', 'last_c',
-                    # 'debut_m', 'last_m',
-                    # 'debut_u', 'last_u',
-                    'bats', 'throws',
-                    'height', 'weight',
-                    'HOF'
-                ]
+        col_dict = {
+            'id': 'id',
+            'lastname': 'last_name',
+            'usename': 'first_name',
+            'fullname': 'full_name',
+            'birthdate': 'birth_date',
+            'birthcity': 'birth_city',
+            'birthstate': 'birth_state',
+            'birthcountry': 'birth_country',
+            'deathdate': 'death_date',
+            'deathcity': 'death_city',
+            'deathstate': 'death_state',
+            'deathcountry': 'death_country',
+            # 'cemetery', 'cem_city', 'cem_state', 'cem_ctry', 'cem_note',
+            'birthname': 'birth_name',
+            'altname': 'alt_name',
+            'debut_p': 'debut_player',
+            'last_p': 'last_player',
+            # 'debut_c', 'last_c',
+            # 'debut_m', 'last_m',
+            # 'debut_u', 'last_u',
+            'bats': 'bats',
+            'throws': 'throws',
+            'height': 'height',
+            'weight': 'weight',
+            'HOF': 'hof',
+        }
 
-                df = df[col_list]
+        df = df[col_dict.keys()]
 
-                df = df.rename(columns={
-                    'id': 'id', 'lastname': 'last_name', 'usename': 'first_name', 'fullname': 'full_name',
-                    'birthdate': 'birth_date', 'birthcity': 'birth_city', 'birthstate': 'birth_state', 'birthcountry': 'birth_country',
-                    'deathdate': 'death_date', 'deathcity': 'death_city', 'deathstate': 'death_state', 'deathcountry': 'death_country',
-                    'birthname': 'birth_name', 'altname': 'alt_name',
-                    'debut_p': 'debut_player', 'last_p': 'last_player',
-                    'bats': 'bats', 'throws': 'throws',
-                    'height': 'height', 'weight': 'weight',
-                    'HOF': 'hof',
-                })
+        df = df.rename(columns=col_dict)
 
-                df.birth_date = convert_num_to_date(df.birth_date)
-                df.death_date = convert_num_to_date(df.death_date)
+        df.birth_date = convert_num_to_date(df.birth_date)
+        df.death_date = convert_num_to_date(df.death_date)
 
-                df.debut_player = convert_num_to_date(df.debut_player)
-                df.last_player = convert_num_to_date(df.last_player)
-        return df
-    def process_teams(self):
-        with self.zip_bytes as z:
+        df.debut_player = convert_num_to_date(df.debut_player)
+        df.last_player = convert_num_to_date(df.last_player)
+        self.df = df
+
+
+class TeamsParser(BaseParser):
+    def __init__(self, blob_name, tbl_dest="teams", write_mode="replace"):
+        super().__init__(self, blob_name, tbl_dest, write_mode)
+
+    def process(self):
+        with self.zip as z:
             # open the csv file in the dataset
             with z.open("teams.csv") as f:
                 df = pd.read_csv(f)
-                df = df.rename(columns={
-                    'TEAM': 'team_id',
-                    'LEAGUE': 'league',
-                    'CITY': 'city',
-                    'NICKNAME': 'nickname',
-                    'FIRST': 'first_year',
-                    'LAST': 'last_year',
-                })
-        return df
-    
-    def process(self):
-        if self.zip_type == 'ballparks':
-            df = self.process_ballparks()
-            return df, 'replace'
-        elif self.zip_type == 'bios':
-            df = self.process_bios()
-            return df, 'replace'
-        elif self.zip_type == 'teams':
-            df = self.process_teams()
-            return df, 'replace'
-        else:
-            raise ValueError(f'Unknown file type given. Recieved {self.zip_type}.')
+        df = df.rename(columns={
+            'TEAM': 'team_id',
+            'LEAGUE': 'league',
+            'CITY': 'city',
+            'NICKNAME': 'nickname',
+            'FIRST': 'first_year',
+            'LAST': 'last_year',
+        })
+        self.df = df
